bd71588f414a25f93c66eb58d72c6fad1314dd9c
mafarthi <mafarthi@ncsu.edu>
2024-01-29 10:46:06 -0500

    reducing unneccessary state, opting for computed values

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,19 +1,4 @@
-const initalGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initalGameBoard;
-
-    for(const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-
+export default function GameBoard({ onSelectSquare, board }) {
     // const [ gameBoard, setGameBoard ] = useState(initalGameBoard);
 
     // function handleSelectSquare(rowIndex, colIndex) {
@@ -28,12 +13,15 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                                <button 
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                                    disabled={playerSymbol !== null}
+                                >
                                     {playerSymbol}
                                 </button>
                             </li>
