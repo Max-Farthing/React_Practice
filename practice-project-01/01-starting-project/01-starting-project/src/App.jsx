0bb3aece2b3431b144ffb19d0fc2fe2e73bb69e3
mafarthi <mafarthi@ncsu.edu>
2024-02-02 12:58:08 -0500

    follwing provided follow along

--- practice-project-01/01-starting-project/01-starting-project/src/App.jsx
+++ practice-project-01/01-starting-project/01-starting-project/src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 function App() {
+  const [ userInput, setUserInput ] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+}); 
+
+function handleChange(inputIdentifier, newValue) {
+  setUserInput(prevUserInput => {
+      const newInput = {...prevUserInput};
+      newInput[inputIdentifier] = +newValue;
+      return newInput;
+  });
+}
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <main>
+        <UserInput 
+          onChangeInput={handleChange} 
+          userInput={userInput}
+        />
+        <Results input={userInput}/>
+      </main>
+    </>
+  );
 }
 
 export default App
