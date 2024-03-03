22030714e9f07ab3725a3bcdb1abf7912305f321
mafarthi <mafarthi@ncsu.edu>
2024-03-02 19:24:32 -0500

    retrying practice calculator

--- practice-project-01/01-starting-project/01-starting-project/src/App.jsx
+++ practice-project-01/01-starting-project/01-starting-project/src/App.jsx
@@ -1,39 +1,15 @@
-import { useState } from "react";
-
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
-import Results from "./components/Results";
 
 function App() {
-  const [ userInput, setUserInput ] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10
-  }); 
-
-  function handleChange(inputIdentifier, newValue) {
-  setUserInput(prevUserInput => {
-        const newInput = {...prevUserInput};
-        newInput[inputIdentifier] = +newValue;
-        return newInput;
-    });
-  }
-
-  const inputIsValid = userInput.duration >= 1;
-
-  return (
-    <>
-      <Header />
-      <main>
-        <UserInput 
-          onChangeInput={handleChange} 
-          userInput={userInput}
-        />
-        {inputIsValid ? <Results input={userInput} /> : <p className="center">Please enter valid duration</p>}
-      </main>
-    </>
-  );
+    return (
+      <>
+          <Header />
+          <main>
+              <UserInput />
+          </main>
+      </>
+    );
 }
 
-export default App
+export default App;
--- practice-project-01/01-starting-project/01-starting-project/src/components/Header.jsx
+++ practice-project-01/01-starting-project/01-starting-project/src/components/Header.jsx
@@ -1,9 +1,9 @@
-import logo from '../assets/investment-calculator-logo.png'
+import money from '../assets/investment-calculator-logo.png';
 
 export default function Header() {
-    return(
-        <header id="header">
-            <img src={logo} alt='money logo image'/>
+    return (
+        <header id='header'>
+            <img src={money}/>
             <h1>React Investment Calculator</h1>
         </header>
     );
