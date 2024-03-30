6907a8f9b14eea88335daddd0b69e1bbd91fbb28
mafarthi <maxfarthing1@gmail.com>
2024-03-30 17:51:48 -0400

    figuring out bug forwarding refs

--- practice-project-02/self-practice/src/App.jsx
+++ practice-project-02/self-practice/src/App.jsx
@@ -1,20 +1,50 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import NoProject from "./components/NoProject";
 import SideBar from "./components/SideBar";
 import Project from "./components/Project";
 
 function App() {
     const [selectedScreen, setSelectedScreen] = useState('none');
+    const [projects, setProjects] = useState([
+        {
+            id: undefined,
+            title: '',
+            description: '',
+            date: '',
+            tasks: []
+        }
+    ]);
+    const projectRefs = useRef({
+        title: null,
+        description: null,
+        date: null
+    });
 
     function addProjectScreen() {
         setSelectedScreen('project');
+    }
+
+    function handleSaveProject() {
+        const newProject = 
+        {
+            id: Math.random(),
+            title: projectRefs.current.title,
+            description: projectRefs.current.description,
+            date: projectRefs.current.date,
+            tasks: []
+        };
+        console.log(newProject);
+        setProjects((prevProjects) => 
+            [newProject, ...prevProjects]
+        );
+        console.log(projects.length);
     }
 
     let screen;
     if(selectedScreen === 'none') {
         screen = <NoProject projectScreen={addProjectScreen} />;
     } else if(selectedScreen === 'project') {
-        screen = <Project />;
+        screen = <Project ref={projectRefs} handleSave={handleSaveProject} />;
     } else if(selectedScreen === 'task') {
         
     }
--- practice-project-02/self-practice/src/components/Project.jsx
+++ practice-project-02/self-practice/src/components/Project.jsx
@@ -1,16 +1,20 @@
-export default function Project() {
+import { forwardRef } from "react";
+
+const Project = forwardRef(function Project({handleSave}, ref) {
     return (
         <div>
            <button className="bg-red-400">Cancel</button>
-           <button className="bg-black text-white">Save</button>
+           <button className="bg-black text-white" onClick={handleSave}>Save</button>
            <div>
                 <label>Title</label> 
-                <input type="text" className="border border-black"/>
+                <input type="text" className="border border-black" ref={ref.title}/>
                 <label>Description</label> 
-                <textarea className="border border-black"/>
+                <textarea className="border border-black" ref={ref.description}/>
                 <label>Due Date</label> 
-                <input type="date" className="border border-black"/>
+                <input type="date" className="border border-black" ref={ref.date}/>
            </div>
         </div>
     );
-}
+});
+
+export default Project;
