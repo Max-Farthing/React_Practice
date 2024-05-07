7b6679799831e9c5c20ee9c176982d9d955cb810
mafarthi <maxfarthing1@gmail.com>
2024-05-07 13:58:38 -0400

    added action creators to interact with backend

--- 14-redux-advanced/src/App.js
+++ 14-redux-advanced/src/App.js
@@ -1,57 +1,49 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux';
-import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart tutorial',
-      }));
-      const response = await fetch(
-        'https://test-13ee9-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        }
-      );
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      if (!response.ok) {
-        throw new Error('Sending cart data failed');
-      }
-
-      const responseData = await response.json();
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sending cart successfully',
-      }));
+  useEffect(() => {
+    if(isInitial) {
+      isInitial = false;
+      return;
     }
 
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart failed',
-      }));
-    });
-  }, [cart, dispatch])
+    if(cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+    
+  }, [cart, dispatch]);
 
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification 
+        status={notification.status} 
+        title={notification.title} 
+        message={notification.message} 
+      /> }
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+    
   );
 };
 
