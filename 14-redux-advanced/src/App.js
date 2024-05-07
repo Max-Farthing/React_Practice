1fcee15fb23da5e69b5b625bb73e2a5ff3160809
mafarthi <maxfarthing1@gmail.com>
2024-05-06 21:12:37 -0400

    firebase backend added

--- 14-redux-advanced/src/App.js
+++ 14-redux-advanced/src/App.js
@@ -1,10 +1,51 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/ui-slice';
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart tutorial',
+      }));
+      const response = await fetch(
+        'https://test-13ee9-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+      const responseData = await response.json();
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sending cart successfully',
+      }));
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart failed',
+      }));
+    });
+  }, [cart, dispatch])
+
 
   return (
     <Layout>
@@ -12,6 +53,10 @@ function App() {
       <Products />
     </Layout>
   );
-}
+};
+
+
+
+
 
 export default App;
